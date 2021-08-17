649a6279781fd07f6d2cff892d8db90f89ce936b
Sumit Kumar <kumar.sumit0619@gmail.com>
2021-08-17 18:23:46 +0530

    first commit from sumit on 17 aug 21

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+import Navbar from './components/Navbar';
+import SectionOne from './components/SectionOne';
+import SectionTwo from './components/SectionTwo';
+import Cards from './components/Cards';
+import CardsTwo from './components/CardsTwo';
+import CardsThree from './components/CardsThree';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <SectionOne />
+      <SectionTwo />
+      <Cards />
+      <CardsTwo />
+      <CardsThree />
     </div>
   );
 }
